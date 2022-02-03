7c5c521255117b284e65ac8a315012fd0ca6a685
kinstugi <kinstugi.webdev@gmail.com>
2022-02-03 19:13:18 +0300

    made some changes

--- lib/pages/search_tab/search_tab_home.dart
+++ lib/pages/search_tab/search_tab_home.dart
@@ -3,6 +3,15 @@ import 'package:yachtmob/widgets/boat_type_tile.dart';
 import 'package:yachtmob/widgets/custom_search_bar.dart';
 import 'package:yachtmob/widgets/destination_card_widget.dart';
 
+//TODO remove later
+final List<Map<String, String>> _dummyData = [
+  {'imageUrl': 'https://bit.ly/3ngYnQ7', 'title': 'Motorboat'},
+  {'imageUrl': 'https://bit.ly/3GljwA7', 'title': 'Sailboat'},
+  {'imageUrl': 'https://bit.ly/3FeVS6R', 'title': 'Catarman'},
+  {'imageUrl': 'https://bit.ly/3GljwA7', 'title': 'Sailboat'},
+  {'imageUrl': 'https://bit.ly/3ngYnQ7', 'title': 'Motorboat'},
+];
+
 class SearchTabHome extends StatelessWidget {
   final VoidCallback onSearchbarTap;
   const SearchTabHome({
@@ -48,26 +57,24 @@ class SearchTabHome extends StatelessWidget {
   Padding _boatsTypeList() {
     return Padding(
       padding: const EdgeInsets.only(left: 18.0),
-      child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        child: Row(
-          children: const [
-            BoatTypeTileWidget(
-              imageUrl: 'https://bit.ly/3ngYnQ7',
-              title: 'Motorboat',
-            ),
-            SizedBox(width: 10),
-            BoatTypeTileWidget(
-              imageUrl: 'https://bit.ly/3GljwA7',
-              title: 'Sailboat',
-            ),
-            SizedBox(width: 10),
-            BoatTypeTileWidget(
-              imageUrl: 'https://bit.ly/3FeVS6R',
-              title: 'Catamaran',
-            ),
-            SizedBox(width: 20),
-          ],
+      child: SizedBox(
+        width: double.infinity,
+        height: 150,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: _dummyData.length,
+          itemBuilder: (ctx, i) {
+            return Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                BoatTypeTileWidget(
+                  imageUrl: _dummyData[i]['imageUrl'] ?? '',
+                  title: _dummyData[i]['title'] ?? '',
+                ),
+                const SizedBox(width: 10)
+              ],
+            );
+          },
         ),
       ),
     );
